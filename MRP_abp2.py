8df8da6679fe654056ba6610b83c525580201076
huffmanm96 <huffmanm96@gmail.com>
2016-06-21 14:44:26 -0400

    Update MRP_abp2.py

--- MRP_abp2.py
+++ MRP_abp2.py
@@ -3,10 +3,10 @@ import op_breakdown
 import SMTPmail
 
 
-m = open('MRP_PrimaryAVList.csv', 'rU')
+m = open('MRP_PrimaryAVList.csv', 'r')
 master = csv.reader(m)
 
-c = open('/Users/maddiehuffman/Desktop/broken/MPC/MRP_PrimaryAVListCopy.csv', 'rU')
+c = open('/Users/maddiehuffman/Desktop/broken/MPC/MRP_PrimaryAVListCopy.csv', 'r')
 copy = csv.reader(c)
 
 master_info = []
@@ -27,7 +27,7 @@ diff_1 = [list(item) for item in set(tuple(row) for row in diff_set)]
 
 all_daily = open('/Users/maddiehuffman/Desktop/broken/MPD/MRPmailandphoneNEW.csv','w')
 ad = csv.writer(all_daily, dialect='excel')
-all_daily.write("BALLOTID,VOTERID,SPOILEDIND,REJECTED,BALLOTNUMBER,BALLOTTYPE,DATESENT,DATERETURNED,APPRETURNED,COUNTY,JURISDICTION,CBPRECINCT,LASTNAME,FIRSTNAME,ADDRESES1,ADDRESS2,ADDRESS3,ADDRESS4,ADDRESS5,SPOILED_BALLOTS,YOB,UOCAVASTATUS \n")
+all_daily.write("HHName,LastName,FirstName,MiddleName,SuffixName,PrimaryAddress1,PrimaryCity,PrimaryState,PrimaryZip,PrimaryOddEvenCode,PrimaryHouseNumber,PrimaryHouseHalf,PrimaryStreetPre,PrimaryStreetName,PrimaryStreetType,PrimaryStreetPost,PrimaryUnit,PrimaryUnitNumber,SecondaryAddress1,SecondaryCity,SecondaryState,SecondaryZip,SecondaryUnit,SecondaryUnitNumber,PrimaryPhone,TelephoneReliabilityCode,EMail,DOB,Gender,ObservedParty,RegistrationDate,GeneralFrequency,PrimaryFrequency,OverAllFrequency,GeneralAbsenteeStatus,PrimaryAbsenteeStatus,CDName,LDName,SDName,CountyName,PrecinctNumber,VoterKey,JurisdictionalVoterId,ClientId,StateVoterId,Latitude,Longitude,MapCode,CellularPhone,HomePhone,OtherPhone,WorkPhone,Township,County Commissioner District \n")
 for a_list in diff_1:
     ad.writerow(a_list)
 all_daily.close()
@@ -35,7 +35,7 @@ m.close()
 c.close()
 
 ###############################################################################
-nmpd = open('/Users/maddiehuffman/Desktop/broken/MPD/MRPmailandphoneNEW.csv','rU')
+nmpd = open('/Users/maddiehuffman/Desktop/broken/MPD/MRPmailandphoneNEW.csv','r')
 new_stuff = csv.reader(nmpd)
 nmps = []
 for line in new_stuff:
@@ -48,17 +48,17 @@ with open('/Users/maddiehuffman/Desktop/broken/MPC/MRP_PrimaryAVListCopy.csv','a
 
 
 ###############################################################################
-og = open('MRP_PrimaryAVList.csv', 'rU')
+og = open('MRP_PrimaryAVList.csv', 'r')
 oginfo = csv.reader(og)
 
 
-ph = open('/Users/maddiehuffman/Desktop/broken/PC/MRPphone.csv', 'rU')
+ph = open('/Users/maddiehuffman/Desktop/broken/PC/MRPphone.csv', 'r')
 old_phone = csv.reader(ph)
 
 
 all_phone_info = []
 for row in oginfo:
-    phone_stuff = (list( row[i] for i in [1, 9, 10, 12, 19, 20] ))
+    phone_stuff = (list( row[i] for i in [36, 37, 38, 39, 40, 52, 53, 1, 2, 3, 5, 24, 25, 43, 44] ))
     all_phone_info.append(phone_stuff)
 
 
@@ -76,7 +76,7 @@ diff_1 = [list(item) for item in set(tuple(row) for row in diff_set)]
 
 newfile = open('/Users/maddiehuffman/Desktop/broken/PD/MRPphoneNEW.csv','w')
 nr = csv.writer(newfile, dialect='excel')
-newfile.write("VOTERID,COUNTY,JURISDICTION,LASTNAME,FIRSTNAME,YOB,UOCAVASTATUS,PHONE#,SECONDARY# \n")
+newfile.write("LastName,FirstName,MiddleName,Address,PrimaryPhone,PhoneReliabilityCode,ClientID,StateVoterID,CD,LD,SD,County,Precinct,Township,County Commissioner District \n")
 for each_list in diff_1:
     nr.writerow(each_list)
 newfile.close()
@@ -84,7 +84,7 @@ og.close()
 ph.close()
 
 ##################################################################################
-np = open('/Users/maddiehuffman/Desktop/broken/PD/MRPphoneNEW.csv','rU')
+np = open('/Users/maddiehuffman/Desktop/broken/PD/MRPphoneNEW.csv','r')
 new_phone_stuff = csv.reader(np)
 nps = []
 for a_line in new_phone_stuff:
@@ -97,17 +97,17 @@ with open('/Users/maddiehuffman/Desktop/broken/PC/MRPphone.csv','a') as phonefil
 
 
 ##################################################################################
-all = open('MRP_PrimaryAVList.csv', 'rU')
+all = open('MRP_PrimaryAVList.csv', 'r')
 allinfo = csv.reader(all)
 
 
-ml = open('/Users/maddiehuffman/Desktop/broken/MC/MRPmail.csv', 'rU')
+ml = open('/Users/maddiehuffman/Desktop/broken/MC/MRPmail.csv', 'r')
 old_mail = csv.reader(ml)
 
 
 all_mail_info = []
 for a_row in allinfo:
-    mail_stuff = (list( a_row[m] for m in [1, 6, 9, 10, 12, 13, 14, 19, 20] ))
+    mail_stuff = (list( a_row[m] for m in [36, 37, 38, 39, 40, 52, 53, 1, 2, 3, 10, 12, 13, 14, 6, 7, 8, 43, 44] ))
     all_mail_info.append(mail_stuff)
 
 
@@ -126,7 +126,7 @@ diff_m = [list(k) for k in set(tuple(r) for r in diff_m_set)]
 
 new_m_file = open('/Users/maddiehuffman/Desktop/broken/MD/MRPmailNEW.csv','w')
 nmr = csv.writer(new_m_file, dialect='excel')
-new_m_file.write("VOTERID,DATESENT,COUNTY,JURISDICTION,FULLNAME,ADDRESS1,ADDRESS2,YOB,UOCAVASTATUS \n")
+new_m_file.write("LastName,FirstName,MiddleName,HouseNumber,StreetPrefix,StreetName,StreetType,City,State,ZipCode,ClientID,StateVoterID,CD,LD,SD,County,Precinct,Township,County Commissioner District \n")
 for each_mlist in diff_m:
     nmr.writerow(each_mlist)
 new_m_file.close()
@@ -134,7 +134,7 @@ all.close()
 ml.close()
 
 ##################################################################################
-nm = open('/Users/maddiehuffman/Desktop/broken/MD/MRPmailNEW.csv','rU')
+nm = open('/Users/maddiehuffman/Desktop/broken/MD/MRPmailNEW.csv','r')
 new_mail_stuff = csv.reader(nm)
 nms = []
 for m_line in new_mail_stuff:
@@ -155,18 +155,37 @@ print (('%s/%s/%s %s:%s:%s') % (now.month, now.day, now.year, now.hour, now.minu
  
  ####################################################################
 
-c_files = ['MC','MPC','PC']
-d_files = ['MD','MPD','PD']
-j_defs = {'CD':1,'CircuitCourt':2,'County':3,'CountyCommission':4,'LD':5,'MCD':6,'Statewide':7}
+c_files = {'MC':'MRPmail','PC':'MRPphone'}
+d_files = {'MD':'MRPmailNEW','PD':'MRPphoneNEW'}
+cdf = 'MPC'
+ddf = 'MPD'
+j_defs = {'CD':1,'LD':2,'SD':3,'County':4,'Precinct':5,'Township':6,'CountyCommissioner':7}
+alt_defs = {'CD':36,'LD':37,'SD':38,'County':39,'Precinct':40,'Township':52,'CountyCommissioner':53}
 
-for each_file in c_files:
-    op_breakdown.breakdown((('/Users/maddiehuffman/Desktop/broken/%s') % (each_file)), each_file, (d for d in j_defs.keys()), (j_defs.get(d)), keep_headers = False)
-for a_file in d_files:
-    op_breakdown.breakdown((('/Users/maddiehuffman/Desktop/broken/%s') % (a_file)), a_file, (d for d in j_defs.keys()), (j_defs.get(d)), keep_headers = True)
+print('Working on c_files')
+for each_file in c_files.keys():
+    for c in j_defs.keys():
+        op_breakdown.breakdown((('/Users/maddiehuffman/Desktop/broken/%s/%s.csv') % (each_file, c_files.get(each_file))), each_file, c, j_defs.get(c), False)
+print('c_files finished')
 
+print('Working on d_files')
+for a_file in d_files.keys():
+    for d in j_defs.keys():
+        op_breakdown.breakdown((('/Users/maddiehuffman/Desktop/broken/%s/%s.csv') % (a_file, d_files.get(a_file))), a_file, d, j_defs.get(d), True)
+print('d_files finished')
+
+print('Working on cumulative mail and phone')
+for a in alt_defs.keys():
+        op_breakdown.breakdown((('/Users/maddiehuffman/Desktop/broken/%s/MRP_PrimaryAVListCopy.csv') % (cdf)), cdf, a, alt_defs.get(a), False)
+print('Cumulative mail and phone finished')
+
+print('Working on new mail and phone')
+for b in alt_defs.keys():
+        op_breakdown.breakdown((('/Users/maddiehuffman/Desktop/broken/%s/MRPmailandphoneNEW.csv') % (ddf)), ddf, b, alt_defs.get(b), True)
+print('New mail and phone finished.')
 ####################################################################
-  
-r = open('Responses.csv','rU')
+print('Starting mailing process')
+r = open('Responses.csv','r')
 responses = csv.reader(r)
 r_list = []
 for line in responses:
@@ -193,15 +212,5 @@ for a_list in cl_r_list:
         filetype = 'MPC'
     SMTPmail.send_mail( 'dnormile@migop.org', a_list[3], (('Absentee Distribution List %s/%s/%s %s:%s:%s') % (now.month, now.day, now.year, now.hour, now.minute, now.second)), "", [(('/Users/maddiehuffman/Desktop/broken/%s/%s/%s_%s_%s.csv') % (filetype,a_list[6],filetype,a_list[6],a_list[7]))])
 
+print('Mailing process finished')
 
-
-
-
-
-
-
-
-
-
-
-  
