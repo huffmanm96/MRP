935dbada1632a4c56ef22084ced4cc392e3c5c06
huffmanm96 <huffmanm96@gmail.com>
2016-06-22 15:29:29 -0400

    Update MRP_abp.py

--- MRP_abp.py
+++ MRP_abp.py
@@ -3,158 +3,158 @@ import op_breakdown
 import SMTPmail
 
 
-# m = open('MRP_PrimaryAVList.csv', 'r')
-# master = csv.reader(m)
-# 
-# c = open('/Users/maddiehuffman/Desktop/broken/MPC/MPC_Statewide_.csv', 'r')
-# copy = csv.reader(c)
-# head = next(copy)
-# 
-# master_info = []
-# for row in master:
-#     mi = (list( row[i] for i in range(len(row) )))
-#     master_info.append(mi)
-# 
-# copy_info = []
-# for line in copy:
-#     ci = (list( line[i] for i in range(len(line) )))
-#     copy_info.append(ci)
-# 
-# first_set = set(map(tuple, master_info))
-# secnd_set = set(map(tuple, copy_info))
-# 
-# diff_set = first_set.symmetric_difference(secnd_set) 
-# diff_1 = [list(item) for item in set(tuple(row) for row in diff_set)]
-# 
-# all_daily = open('/Users/maddiehuffman/Desktop/broken/MPD/MPD_Statewide_.csv','w')
-# ad = csv.writer(all_daily, dialect='excel')
-# ad.writerow(head)
-# for a_list in diff_1:
-#     ad.writerow(a_list)
-# 
-# 
-# ###############################################################################
-# nmpd = open('/Users/maddiehuffman/Desktop/broken/MPD/MPD_Statewide_.csv','r')
-# new_stuff = csv.reader(nmpd)
-# nmps = []
-# for line in new_stuff:
-#     nmps.append(line)
-# with open('/Users/maddiehuffman/Desktop/broken/MPC/MPC_Statewide_.csv','a') as copyfile:
-#     copyfile.write('\n')
-#     for a_list in nmps[1:]:
-#         wr = csv.writer(copyfile, dialect='excel')
-#         wr.writerow(a_list)
-# 
-# 
-# ###############################################################################
-# og = open('MRP_PrimaryAVList.csv', 'r')
-# oginfo = csv.reader(og)
-# 
-# 
-# ph = open('/Users/maddiehuffman/Desktop/broken/PC/MRPphone.csv', 'r')
-# old_phone = csv.reader(ph)
-# 
-# 
-# all_phone_info = []
-# for row in oginfo:
-#     phone_stuff = (list( row[i] for i in [36, 37, 38, 39, 40, 52, 53, 1, 2, 3, 5, 24, 25, 43, 44] ))
-#     all_phone_info.append(phone_stuff)
-# 
-# 
-# old_phone_info = []
-# for line in old_phone:
-#     old_phone_info.append(line)
-# 
-# 
-# first_set = set(map(tuple, all_phone_info))
-# secnd_set = set(map(tuple, old_phone_info))
-# 
-# 
-# diff_set = first_set.symmetric_difference(secnd_set) 
-# diff_1 = [list(item) for item in set(tuple(row) for row in diff_set)]
-# 
-# newfile = open('/Users/maddiehuffman/Desktop/broken/PD/MRPphoneNEW.csv','w')
-# nr = csv.writer(newfile, dialect='excel')
-# newfile.write("LastName,FirstName,MiddleName,Address,PrimaryPhone,PhoneReliabilityCode,ClientID,StateVoterID,CD,LD,SD,County,Precinct,Township,County Commissioner District \n")
-# for each_list in diff_1:
-#     nr.writerow(each_list)
-# newfile.close()
-# og.close()
-# ph.close()
-# 
-# ##################################################################################
-# np = open('/Users/maddiehuffman/Desktop/broken/PD/MRPphoneNEW.csv','r')
-# new_phone_stuff = csv.reader(np)
-# nps = []
-# for a_line in new_phone_stuff:
-#     nps.append(a_line)
-# with open('/Users/maddiehuffman/Desktop/broken/PC/MRPphone.csv','a') as phonefile:
-#     phonefile.write('\n')
-#     for a_list in nps[1:]:
-#         wr = csv.writer(phonefile, dialect='excel')
-#         wr.writerow(a_list)
-# 
-# 
-# ##################################################################################
-# all = open('MRP_PrimaryAVList.csv', 'r')
-# allinfo = csv.reader(all)
-# 
-# 
-# ml = open('/Users/maddiehuffman/Desktop/broken/MC/MRPmail.csv', 'r')
-# old_mail = csv.reader(ml)
-# 
-# 
-# all_mail_info = []
-# for a_row in allinfo:
-#     mail_stuff = (list( a_row[m] for m in [36, 37, 38, 39, 40, 52, 53, 1, 2, 3, 10, 12, 13, 14, 6, 7, 8, 43, 44] ))
-#     all_mail_info.append(mail_stuff)
-# 
-# 
-# old_mail_info = []
-# for each_line in old_mail:
-#     old_mail_info.append(each_line)
-# 
-# 
-# 
-# first_m_set = set(map(tuple, all_mail_info))
-# secnd_m_set = set(map(tuple, old_mail_info))
-# 
-# 
-# diff_m_set = first_m_set.symmetric_difference(secnd_m_set) 
-# diff_m = [list(k) for k in set(tuple(r) for r in diff_m_set)]
-# 
-# new_m_file = open('/Users/maddiehuffman/Desktop/broken/MD/MRPmailNEW.csv','w')
-# nmr = csv.writer(new_m_file, dialect='excel')
-# new_m_file.write("LastName,FirstName,MiddleName,HouseNumber,StreetPrefix,StreetName,StreetType,City,State,ZipCode,ClientID,StateVoterID,CD,LD,SD,County,Precinct,Township,County Commissioner District \n")
-# for each_mlist in diff_m:
-#     nmr.writerow(each_mlist)
-# new_m_file.close()
-# all.close()
-# ml.close()
-# 
-# ##################################################################################
-# nm = open('/Users/maddiehuffman/Desktop/broken/MD/MRPmailNEW.csv','r')
-# new_mail_stuff = csv.reader(nm)
-# nms = []
-# for m_line in new_mail_stuff:
-#     nms.append(m_line)
-# with open('/Users/maddiehuffman/Desktop/broken/MC/MRPmail.csv','a') as mailfile:
-#     mailfile.write('\n')
-#     for m_list in nms[1:]:
-#         mr = csv.writer(mailfile, dialect='excel')
-#         mr.writerow(m_list)
-# 
-# 
-# 
-# from datetime import datetime
-# now = datetime.now()
-# 
-# print (('%s/%s/%s %s:%s:%s') % (now.month, now.day, now.year, now.hour, now.minute, now.second))
-# 
-#  
-#  ####################################################################
-# 
-# 
+m = open('MRP_PrimaryAVList.csv', 'r')
+master = csv.reader(m)
+
+c = open('/Users/maddiehuffman/Desktop/broken/MPC/MPC_Statewide_.csv', 'r')
+copy = csv.reader(c)
+head = next(copy)
+
+master_info = []
+for row in master:
+    mi = (list( row[i] for i in range(len(row) )))
+    master_info.append(mi)
+
+copy_info = []
+for line in copy:
+    ci = (list( line[i] for i in range(len(line) )))
+    copy_info.append(ci)
+
+first_set = set(map(tuple, master_info))
+secnd_set = set(map(tuple, copy_info))
+
+diff_set = first_set.symmetric_difference(secnd_set) 
+diff_1 = [list(item) for item in set(tuple(row) for row in diff_set)]
+
+all_daily = open('/Users/maddiehuffman/Desktop/broken/MPD/MPD_Statewide_.csv','w')
+ad = csv.writer(all_daily, dialect='excel')
+ad.writerow(head)
+for a_list in diff_1:
+    ad.writerow(a_list)
+
+
+###############################################################################
+nmpd = open('/Users/maddiehuffman/Desktop/broken/MPD/MPD_Statewide_.csv','r')
+new_stuff = csv.reader(nmpd)
+nmps = []
+for line in new_stuff:
+    nmps.append(line)
+with open('/Users/maddiehuffman/Desktop/broken/MPC/MPC_Statewide_.csv','a') as copyfile:
+    copyfile.write('\n')
+    for a_list in nmps[1:]:
+        wr = csv.writer(copyfile, dialect='excel')
+        wr.writerow(a_list)
+
+
+###############################################################################
+og = open('MRP_PrimaryAVList.csv', 'r')
+oginfo = csv.reader(og)
+
+
+ph = open('/Users/maddiehuffman/Desktop/broken/PC/MRPphone.csv', 'r')
+old_phone = csv.reader(ph)
+
+
+all_phone_info = []
+for row in oginfo:
+    phone_stuff = (list( row[i] for i in [36, 37, 38, 39, 40, 52, 53, 1, 2, 3, 5, 24, 25, 43, 44] ))
+    all_phone_info.append(phone_stuff)
+
+
+old_phone_info = []
+for line in old_phone:
+    old_phone_info.append(line)
+
+
+first_set = set(map(tuple, all_phone_info))
+secnd_set = set(map(tuple, old_phone_info))
+
+
+diff_set = first_set.symmetric_difference(secnd_set) 
+diff_1 = [list(item) for item in set(tuple(row) for row in diff_set)]
+
+newfile = open('/Users/maddiehuffman/Desktop/broken/PD/MRPphoneNEW.csv','w')
+nr = csv.writer(newfile, dialect='excel')
+newfile.write("LastName,FirstName,MiddleName,Address,PrimaryPhone,PhoneReliabilityCode,ClientID,StateVoterID,CD,LD,SD,County,Precinct,Township,County Commissioner District \n")
+for each_list in diff_1:
+    nr.writerow(each_list)
+newfile.close()
+og.close()
+ph.close()
+
+##################################################################################
+np = open('/Users/maddiehuffman/Desktop/broken/PD/MRPphoneNEW.csv','r')
+new_phone_stuff = csv.reader(np)
+nps = []
+for a_line in new_phone_stuff:
+    nps.append(a_line)
+with open('/Users/maddiehuffman/Desktop/broken/PC/MRPphone.csv','a') as phonefile:
+    phonefile.write('\n')
+    for a_list in nps[1:]:
+        wr = csv.writer(phonefile, dialect='excel')
+        wr.writerow(a_list)
+
+
+##################################################################################
+all = open('MRP_PrimaryAVList.csv', 'r')
+allinfo = csv.reader(all)
+
+
+ml = open('/Users/maddiehuffman/Desktop/broken/MC/MRPmail.csv', 'r')
+old_mail = csv.reader(ml)
+
+
+all_mail_info = []
+for a_row in allinfo:
+    mail_stuff = (list( a_row[m] for m in [36, 37, 38, 39, 40, 52, 53, 1, 2, 3, 10, 12, 13, 14, 6, 7, 8, 43, 44] ))
+    all_mail_info.append(mail_stuff)
+
+
+old_mail_info = []
+for each_line in old_mail:
+    old_mail_info.append(each_line)
+
+
+
+first_m_set = set(map(tuple, all_mail_info))
+secnd_m_set = set(map(tuple, old_mail_info))
+
+
+diff_m_set = first_m_set.symmetric_difference(secnd_m_set) 
+diff_m = [list(k) for k in set(tuple(r) for r in diff_m_set)]
+
+new_m_file = open('/Users/maddiehuffman/Desktop/broken/MD/MRPmailNEW.csv','w')
+nmr = csv.writer(new_m_file, dialect='excel')
+new_m_file.write("LastName,FirstName,MiddleName,HouseNumber,StreetPrefix,StreetName,StreetType,City,State,ZipCode,ClientID,StateVoterID,CD,LD,SD,County,Precinct,Township,County Commissioner District \n")
+for each_mlist in diff_m:
+    nmr.writerow(each_mlist)
+new_m_file.close()
+all.close()
+ml.close()
+
+##################################################################################
+nm = open('/Users/maddiehuffman/Desktop/broken/MD/MRPmailNEW.csv','r')
+new_mail_stuff = csv.reader(nm)
+nms = []
+for m_line in new_mail_stuff:
+    nms.append(m_line)
+with open('/Users/maddiehuffman/Desktop/broken/MC/MRPmail.csv','a') as mailfile:
+    mailfile.write('\n')
+    for m_list in nms[1:]:
+        mr = csv.writer(mailfile, dialect='excel')
+        mr.writerow(m_list)
+
+
+
+from datetime import datetime
+now = datetime.now()
+
+print (('%s/%s/%s %s:%s:%s') % (now.month, now.day, now.year, now.hour, now.minute, now.second))
+
+ 
+ ####################################################################
+
+
 j_defs = {'CD':0,'LD':1,'SD':2,'County':3,'Precinct':4,'Township':5,'CountyCommissioner':6}
 # # alt_defs = {'CD':36,'LD':37,'SD':38,'County':39,'Precinct':40,'Township':52,'CountyCommissioner':53}
 # 
